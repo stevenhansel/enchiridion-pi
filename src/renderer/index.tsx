3881a95a69abf0eee779878f4122df9cea0b1d34
stevenhansel <stevenhanselgo@gmail.com>
2022-05-12 17:44:04 +0700

    chore: initialize rust tauri + yew project

--- src/renderer/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const container = document.getElementById('root')!;
-const root = createRoot(container);
-root.render(<App />);
-
-// window.electron.ipcRenderer.invoke('get-user-path').then((path) => console.log('path:', path))
