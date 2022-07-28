036219366acf271ff914d4cc3ba19b5271b140a0
stevenhansel <stevenhanselgo@gmail.com>
2022-07-28 18:33:17 +0700

    ci: add logs

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ function App() {
 
   const getAnnouncementMedias = useCallback(async () => {
     try {
+      console.log('getting images');
       const images = await tauri.getImages();
 
       setImages(images);
