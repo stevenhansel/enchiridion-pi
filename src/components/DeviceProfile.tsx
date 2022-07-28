fff15205cb15eb63145be510c5fb0a86aa2e360e
stevenhansel <stevenhanselgo@gmail.com>
2022-07-28 16:04:06 +0700

    feat: finish device profile

--- src/components/DeviceProfile.tsx
+++ src/components/DeviceProfile.tsx
@@ -2,15 +2,24 @@ import React, { useContext } from 'react';
 import { MenuContext, MenuOptions } from './constants';
 
 const DeviceProfile = () => {
-  const { setActiveMenu } = useContext(MenuContext);
+  const { deviceInformation, setActiveMenu } = useContext(MenuContext);
 
   return (
     <div>
       <h2>Device Information</h2>
 
-      <div onClick={() => setActiveMenu(MenuOptions.MainMenu)}>
+      {deviceInformation !== null ? (
+          <>
+            <p>Device ID: {deviceInformation.id}</p>
+            <p>Device Name: {deviceInformation.name}</p>
+            <p>Device Location: {deviceInformation.location}</p>
+            <p>Device Description: {deviceInformation.description}</p>
+          </>
+      ) : (<p>Device has not been registered in the system</p>)}
+
+      <button onClick={() => setActiveMenu(MenuOptions.MainMenu)}>
         <p>Back</p>
-      </div>
+      </button>
     </div>
   )
 };
--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -4,17 +4,20 @@ import './MainMenu.css';
 import { MenuContext, MenuOptions } from './constants';
 
 const MainMenu = () => {
-  const { close, setActiveMenu } = useContext(MenuContext);
+  const { deviceInformation, close, setActiveMenu } = useContext(MenuContext);
 
   return (
     <div>
-      <div className="menu-item" onClick={() => setActiveMenu(MenuOptions.DeviceInformation)}>
-        <p>Device Information</p>
-      </div>
+      {deviceInformation !== null ? (
+        <div className="menu-item" onClick={() => setActiveMenu(MenuOptions.DeviceProfile)}>
+          <p>Profile</p>
+        </div>
+      ) : (
+        <div className="menu-item" onClick={() => setActiveMenu(MenuOptions.Registration)}>
+          <p>Registration</p>
+        </div>
+      )}
 
-      <div className="menu-item" onClick={() => setActiveMenu(MenuOptions.Registration)}>
-        <p>Registration</p>
-      </div>
 
       <div className="menu-item" onClick={() => close()}>
         <p>Close</p>
